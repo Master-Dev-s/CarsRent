676b3f031ce7493bbe389f2c0414a570648a3cfe
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-05-09 22:18:46 -0300

    feat: criado projeto springboot, realizado conexão com banco mysql e a dockerização do projeto!

---
+++ backend/carsrent/src/main/java/com/digitalhouse/carsrent/CarsrentApplication.java
@@ -0,0 +1,18 @@
+package com.digitalhouse.carsrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsrentApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(
+                CarsrentApplication.class,
+                args
+                             );
+    }
+
+}
