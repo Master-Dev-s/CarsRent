637e1bfc66f22f8c27fecbdaa81bab93b0f695e7
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-05-15 23:03:56 -0300

    feat: ajustado configuracao de cors e jwt

--- backend/carsrent/src/main/java/com/digitalhouse/carsrent/utils/JwtUtil.java
+++ backend/carsrent/src/main/java/com/digitalhouse/carsrent/utils/JwtUtil.java
@@ -3,6 +3,7 @@ package com.digitalhouse.carsrent.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -11,13 +12,16 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    private String secret = "yourSecretKey";
-    private int expirationInMilliseconds = 86400000; // 24 hours
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private int expirationInMilliseconds;
 
     public String generateToken(String username) {
         return Jwts.builder()
                    .setSubject(username)
-                   .setIssuedAt(new Date(System.currentTimeMillis()))
+                   .setIssuedAt(new Date())
                    .setExpiration(new Date(System.currentTimeMillis() + expirationInMilliseconds))
                    .signWith(SignatureAlgorithm.HS256, secret)
                    .compact();
